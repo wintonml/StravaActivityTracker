1e911051007372755509cf0b0f333729daf08474
Michael Lennon Winton <wintonml@gmail.com>
2022-10-17 21:47:30 +1300

    1: Obtain basic user information using Strava APIs
    After entering an access token into the program. It will now obtain the users' name and print it out

---
+++ StravaActivities/StravaActivities/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace StravaActivities
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Enter access token");
+            string access_token = Console.ReadLine();
+            StravaInformation strava = new StravaInformation(access_token);
+            Athlete test = await strava.GetAthlete();
+            Console.WriteLine($"This Access Token belongs to: {test.FirstName} {test.LastName}");
+        }
+    }
+}
